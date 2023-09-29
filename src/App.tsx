414fedb7dc3c2cee609dfe2d511ef5181f564ade
Santona Subedi <santonasubedi@gmail.com>
2023-09-28 20:53:31 -0500

    promise in js

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,49 @@
-import { useEffect, useState } from "react";
+//api url ->https://restcountries.com/v3.1/all
+//fetch() gives us output and also takes input and gives us promise
+//promise is an object which holds the result of async operation
+// ways to handle promise
+//1. using then method (anything inside then is async and will execute at end)
+//2. async await
+
+const apiURL = "https://restcountries.com/v3.1/all";
+const result = fetch(apiURL);
+
+console.log("Before handling promise"); //1 st run
+result
+  .then((response) => {
+    console.log("Inside first then"); //4th run
+    return response.json();
+  })
+  .then((data) => {
+    console.log("Inside second then"); //5th run
+  });
+
+//Block code: weather call stack blocked or not
+for (let i = 0; i < 100; i++) {
+  console.log("Running loop"); // 2nd run
+}
+console.log("After handling promise"); //3rd run
+
+//refined above line 12 to 17 in line 21
+//result.then((response) => response.json().then((data) => console.log(data)));
 
 function App() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [catData, setCatData] = useState([]);
-  useEffect(() => {
-    //fetch data from database
-
-    // (response) => response.json(); -> asynchronous code
-    // (data) => data; -> asynchronous code
-
-    //connecting with the database
-    const promiseOne = fetch("https://api.thecatapi.com/v1/images/search");
-    const promiseTwo = promiseOne.then((response) => response.json());
-    promiseTwo.then((data) => {
-      //data is received here and our application doesn't know about it
-      setCatData(data); //here we are changing the data
-      setIsLoading(false);
-    });
-  }, []); //[]-> this here means useEffect hook can only run once at the end. even if the component is renders/re-renders
-  return isLoading ? (
-    <div>Getting cat data</div>
-  ) : (
-    catData.map((cat) => <img key={cat.id} src={cat.url} />)
-  );
+  return <div>App</div>;
 }
 export default App;
+
+/*
+const obj = {
+  anything: () => {
+    console.log("I am anything function");
+  },
+};
+obj.anything();
+*/
+
+/*
+const numbers = [1, 2, 3, 4];
+const newNumbers = numbers.map((num) => {
+  console.log(num * 10);
+});
+*/
