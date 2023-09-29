7306d5eb8e918ae1ec89a4446efdcc077ce8c4ec
Santona Subedi <santonasubedi@gmail.com>
2023-09-28 19:21:06 -0500

    useEffect and API

--- src/App.tsx
+++ src/App.tsx
@@ -1,82 +1,27 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
-  console.log("1. App component rendering started.");
-  //useState(); // has both input and output
+  const [isLoading, setIsLoading] = useState(true);
+  const [catData, setCatData] = useState([]);
+  useEffect(() => {
+    //fetch data from database
 
-  //Use effect
-  //provide input and no output -> input is the callback function but we only call here
-  // and the other input can be the array []-> dependency array
+    // (response) => response.json(); -> asynchronous code
+    // (data) => data; -> asynchronous code
 
-  const callbackFn = () => {
-    console.log("2. Running from the useEffect hook");
-  };
-
-  //asynchronous code always at the end -> asynchronous
-  // setTimeout(() => {
-  //   setCount((prevState) => {
-  //     return prevState + 1;
-  //   });
-  // }, 5000);
-
-  // useEffect hook will run out at the end of the component rendering or (re-rendering) process.
-  useEffect(callbackFn);
-  console.log("3. Running below useEffect hook call");
-  return (
-    <>
-      {console.log("4. Running inside jsx")}
-      <div>Hello</div>
-      <h1>{count}</h1>
-      <button
-        onClick={() => {
-          // setCount is the call back function
-          setCount((prevState) => prevState + 1);
-        }}
-      >
-        Update Number
-      </button>
-    </>
+    //connecting with the database
+    const promiseOne = fetch("https://api.thecatapi.com/v1/images/search");
+    const promiseTwo = promiseOne.then((response) => response.json());
+    promiseTwo.then((data) => {
+      //data is received here and our application doesn't know about it
+      setCatData(data); //here we are changing the data
+      setIsLoading(false);
+    });
+  }, []); //[]-> this here means useEffect hook can only run once at the end. even if the component is renders/re-renders
+  return isLoading ? (
+    <div>Getting cat data</div>
+  ) : (
+    catData.map((cat) => <img key={cat.id} src={cat.url} />)
   );
 }
-
 export default App;
-
-// when you print it: 1,3,4,2
-
-//function -> no input , output provide
-//function without return will give undefined
-/*
-function add() {
-  const a = 10;
-  const b = 20;
-  return a + b;
-}
-*/
-
-//input , output -> yes
-/*
-function add(a:number,b: number): number -> this is for output
-{
-  return a+b
-}
-*/
-
-//no input, no output
-/*
-function add()
-{
-  const a =10;
-  const b =20;
-
-  console.log(a,b)
-}
-*/
-
-//input , no output
-/*
-function add(a: number,b:number)
-{
-console.log(a,b)
-}
-*/
